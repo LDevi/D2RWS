e5b1d9551da99c30d72e17a0c932e4fad072878a
LDevi <LDevi@users.noreply.github.com>
2018-05-21 20:35:19 +0200

    [1.0.0][#1][runes] List of all runes

---
+++ 2-android-feature-runes/src/main/kotlin/ldev/net/d2/app/assistant/android/feature/runes/extention/RuneExtensions.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2018 Louis Devineau.
+ * This file is part of Diablo-2-App-Assistant.
+ *
+ *     Diablo-2-App-Assistant is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Diablo-2-App-Assistant is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Diablo-2-App-Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ldev.net.d2.app.assistant.android.feature.runes.extention
+
+import android.content.Context
+import ldev.net.d2.items.core.entity.Rune
+
+fun Rune.toIcon(context: Context): Int = context.resources.getIdentifier("ic_" + id + "_" + name.toLowerCase(), "drawable", context.packageName)
---
+++ 2-android-feature-runes/src/main/kotlin/ldev/net/d2/app/assistant/android/feature/runes/list/RuneListActivity.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2018 Louis Devineau.
+ * This file is part of Diablo-2-App-Assistant.
+ *
+ *     Diablo-2-App-Assistant is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Diablo-2-App-Assistant is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Diablo-2-App-Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ldev.net.d2.app.assistant.android.feature.runes.list
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_socketable_list.*
+import kotlinx.android.synthetic.main.list_item_socketable_view.view.*
+import ldev.net.d2.app.assistant.android.feature.R
+import ldev.net.d2.app.assistant.android.feature.runes.di.dagger.tools.ViewModelFactory
+import ldev.net.d2.app.assistant.android.feature.runes.extention.toIcon
+import ldev.net.d2.app.assistant.android.resources.extension.android.inflate
+import ldev.net.d2.items.core.entity.Rune
+import org.jetbrains.anko.doAsync
+import javax.inject.Inject
+
+class RuneListActivity : AppCompatActivity() {
+    @Inject
+    protected lateinit var runeListViewModelFactory: ViewModelFactory<RuneListViewModel>
+    private lateinit var runeListViewModel: RuneListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
+        super.onCreate(savedInstanceState)
+        runeListViewModel = ViewModelProviders.of(this, runeListViewModelFactory).get(RuneListViewModel::
+        class.java)
+        setContentView(R.layout.activity_socketable_list)
+
+        runesRecyclerView.adapter = SocketableAdapter()
+        runeListViewModel.runeList.observe(this, Observer {
+            if (it != null) {
+                (runesRecyclerView.adapter as SocketableAdapter).updateData(it)
+            }
+        })
+        doAsync {
+            runeListViewModel.loadRunes()
+        }
+
+    }
+}
+
+class SocketableAdapter(private var runeList: List<Rune> = ArrayList()) : RecyclerView.Adapter<SocketableAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(runeList[position])
+    }
+
+    override fun getItemCount(): Int = runeList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(parent.inflate(R.layout.list_item_socketable_view))
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(rune: Rune) = with(itemView) {
+            runeIcon.setImageResource(rune.toIcon(context))
+            runeName.text = rune.name
+        }
+    }
+
+    fun updateData(runeList: List<Rune>) {
+        this.runeList = runeList
+        notifyDataSetChanged()
+    }
+
+}
+
