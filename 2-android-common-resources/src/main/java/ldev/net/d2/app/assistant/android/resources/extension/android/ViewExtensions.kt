8e28190b03c7a2a57c2fd38771474bd5e7d0ac7a
LDevi <LDevi@users.noreply.github.com>
2018-05-21 20:32:08 +0200

    [1.0.0][technical]Adding shared resources module

---
+++ 2-android-common-resources/src/main/java/ldev/net/d2/app/assistant/android/resources/extension/android/ViewExtensions.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Louis Devineau.
+ * This file is part of Diablo-2-App-Assistant.
+ *
+ *     Diablo-2-App-Assistant is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Diablo-2-App-Assistant is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Diablo-2-App-Assistant.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ldev.net.d2.app.assistant.android.resources.extension.android
+
+import android.view.View
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
